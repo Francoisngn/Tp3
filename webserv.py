641582845549b864bacfb0ba05ab608a8f4f9961
Francoisngn <50597513+Francoisngn@users.noreply.github.com>
2022-03-22 07:01:40 +0100

    Update webserv.py

--- webserv.py
+++ webserv.py
@@ -1,9 +1,44 @@
-from imp import reload
-from typing import Optional
-from fastapi import FastAPI
+import requests
+from PyQt5.QtWidgets import QMessageBox
 
-app = FastAPI()
+class Main():
+    def query (self,host)
+    r= requests.get(url)
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+    if r.status_code == requests.codes.NOT_FOUND
+        QMessageBox.about( self,"bug", "ip not found")
+    if r.status_code == requests.codes.OK :
+        return r.json()
+
+if __name__  == "__main__":
+    main = Main() 
+    hostname= "127.0.0.1:8000"
+    res = main.query(hostname)
+
+    if res:
+        print (res)
+
+from shodan import Shodan 
+
+ #@app.get("/ip/{ip}")
+    async def get_ip(ip: str, key: Optional[str] =None)
+  #   return"Hello": "World"}
+
+
+def __query (self, hostname):
+    url = "http://%s % (hostname)
+    r= request.get(url)
+
+    if r.status _code  == request.codes.NOT_FOUND:
+        QMessageBox.about( self, "Error", "IP not found")
+
+    if r.status_code == requests.codes.NOT_FOUND;
+        QMessageBox.about(self, "Error", "IP not found")
+
+    if r.status_code == requests.codes.NOT_FOUND/:
+        QMessageBox.about(self, "Error", "IP not found")
+
+    if r.status_code == requests.codes.Ok:
+        return r.json()
+
+
